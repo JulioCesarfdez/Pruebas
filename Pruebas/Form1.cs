d3b7245740fcd43be54368d6492fa85df42b8d3d
julio cesar fernandez hernandez <cesarfdez_-_@hotmail.com>
2021-02-23 15:06:46 -0600

    Primer Push

---
+++ Pruebas/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pruebas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            panel3.Visible = panel3.Visible? false:true;            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            remplazarForm(new FormulariaRemplazar1());
+        }
+
+
+        Form formRemplazar = null;
+        private void remplazarForm(Form formulario)
+        {
+            //if (formRemplazar != null) formRemplazar.Close();
+            formulario.TopLevel = false;
+            formRemplazar = formulario;
+            panel4.Controls.Add(formulario);
+            formulario.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (formRemplazar != null) formRemplazar.Close();
+        }
+    }
+}
